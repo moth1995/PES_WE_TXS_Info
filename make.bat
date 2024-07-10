d9fbb21a2af8aa78e9616122c5542d05a38ffb57
moth1995 <marcos-flores@live.com.ar>
2024-07-10 19:38:07 -0300

    fixing make file

--- make.bat
+++ make.bat
@@ -1,19 +1,12 @@
 @echo on
-set PY_FILE=text_editor.py
-set PROJECT_NAME=PES WE J League Text Editor
-set VERSION=1.1.0
+set PY_FILE=pes_we_txs_info.py
+set PROJECT_NAME=TXS Info
+set VERSION=1.0.0
 set FILE_VERSION=file_version_info.txt
-set EXTRA_ARG=--add-data=resources/*;resources/ 
 set ICO_DIR=resources/pes_indie.ico
 
-pyinstaller --onefile --window "%PY_FILE%" --icon="%ICO_DIR%" --name "%PROJECT_NAME%_%VERSION%"  %EXTRA_ARG% --version-file "%FILE_VERSION%"
-
-Rem This command below is just specific for this script
-
-Xcopy /E ".\config\" ".\dist\config\"
-
-Rem end of extra command
+pyinstaller --onefile "%PY_FILE%" --icon="%ICO_DIR%" --name "%PROJECT_NAME%_%VERSION%"  %EXTRA_ARG% --version-file "%FILE_VERSION%"
 
 cd dist
-tar -acvf "%PROJECT_NAME%_%VERSION%.zip" * config
+tar -acvf "%PROJECT_NAME%_%VERSION%.zip" *
 pause
